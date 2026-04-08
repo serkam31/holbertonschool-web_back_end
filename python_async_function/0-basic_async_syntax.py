3486a3fa1d89e38b2f48cc68e0c99bfb782a0944
Writingway <sendmeat4time@gmail.com>
2026-04-08 10:30:19 +0200

    fix: update shebang for portability and adjust type hint for max_delay in wait_random function

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,16 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import random
 import asyncio
 
 
-async def wait_random(max_delay: float = 10.0) -> float:
-    """Wait for a random delay between 0 and max_delay seconds.
-
+async def wait_random(max_delay: int = 10) -> float:
+    """Wait for a random delay between 0 and max_delay seconds and return it.
     Args:
-        max_delay (float): The maximum delay in seconds (default is 10.0).
-
-    Returns:
-        float: The actual delay in seconds.
+        max_delay (int): The maximum delay in seconds (default is 10).
+        Returns:
+            float: The actual delay in seconds.
     """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
