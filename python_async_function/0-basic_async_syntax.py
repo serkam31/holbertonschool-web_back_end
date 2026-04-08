0be38c034a4415fce60fa85aae862865fc379123
Writingway <sendmeat4time@gmail.com>
2026-04-08 10:31:17 +0200

    fix: add docstring to clarify the purpose of the wait_random function

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+Function that takes an integer argument (max_delay, with a default value of 10)
+and returns a float.
+"""
 import random
 import asyncio
 
@@ -7,8 +11,8 @@ async def wait_random(max_delay: int = 10) -> float:
     """Wait for a random delay between 0 and max_delay seconds and return it.
     Args:
         max_delay (int): The maximum delay in seconds (default is 10).
-        Returns:
-            float: The actual delay in seconds.
+    Returns:
+        float: The actual delay in seconds.
     """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
