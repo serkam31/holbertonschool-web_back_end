eab9f8dbe8807f88a4f2043ffb4fd06368870f2f
Writingway <sendmeat4time@gmail.com>
2026-04-08 09:58:50 +0200

    fix: update type hint for mxd_lst in sum_mixed_list function to use List from typing

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -3,9 +3,9 @@
 This module defines a function `sum_mixed_list` that takes a list of integers
 and floats as an argument and returns their sum as a float.
 """
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Returns the sum of a list of floats and integers as a float."""
     return sum(mxd_lst)
