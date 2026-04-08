42f4aab7e2559b9e2276d40bff39f1a54319fbe4
Writingway <sendmeat4time@gmail.com>
2026-04-08 09:47:51 +0200

    fix: update type hint for input_list in sum_list function to use List from typing

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,8 +3,9 @@
 This module defines a function `sum_list` that takes a list of floats as an argument
 and returns their sum as a float.
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Returns the sum of a list of floats as a float."""
     return sum(input_list)
