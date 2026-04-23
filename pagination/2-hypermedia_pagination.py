70cac30067ecd6f5f085016846449743ddd7facf
Matéo MARQUES <mmateomarques@gmail.com>
2026-04-23 12:23:19 +0000

    feat: implement hypermedia pagination with get_hyper method

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""1-simple_pagination.py"""
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """Return a tuple of start and end indexes for pagination."""
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return the appropriate page of the dataset."""
+        assert (
+            isinstance(page, int)
+            and isinstance(page_size, int)
+            and page > 0
+            and page_size > 0
+        )
+
+        start, end = index_range(page, page_size)
+        return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """return a dictionnary"""
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages,
+        }
