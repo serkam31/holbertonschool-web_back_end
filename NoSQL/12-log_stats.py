cbfcb4ec7efc3ff28de360c548f206301d60b220
Matéo MARQUES <mmateomarques@gmail.com>
2026-04-24 14:00:25 +0000

    refactor: simplify log stats display by removing unnecessary function

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python3
-"""Provide some stats about Nginx logs stored in MongoDB."""
-
+"""Display stats about Nginx logs stored in MongoDB."""
 from pymongo import MongoClient
 
 
-def log_stats(mongo_collection):
-    """Print stats about Nginx logs stored in MongoDB."""
-    total = mongo_collection.count_documents({})
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-    print("{total} logs".format(total=total))
-    print("Methods:")
-    for method in methods:
-        count = mongo_collection.count_documents({"method": method})
-        print("\tmethod {method}: {count}".format(method=method, count=count))
-
-    status_check_count = mongo_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print("{count} status check".format(count=status_check_count))
-
-
 if __name__ == "__main__":
-    client = MongoClient("mongodb://127.0.0.1:27017")
-    log_stats(client.logs.nginx)
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    total = collection.count_documents({})
+    print("{} logs".format(total))
+
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
+
+    status = collection.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(status))
