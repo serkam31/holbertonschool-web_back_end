fff4caa5e80f981620a7cf6c488c54209f8714c4
serkam31 <mmateomarques@gmail.com>
2026-05-12 13:15:09 +0200

    fix: standardize string formatting in 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.resume();
 process.stdin.on('data', (data) => {
-  process.stdout.write('Your name is: ' + data.toString());
+  process.stdout.write(`Your name is: ${data.toString()}`);
 });
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
