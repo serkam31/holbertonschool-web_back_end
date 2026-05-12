59a92e91c879491c9c33471fce6f1c50910f74ac
serkam31 <mmateomarques@gmail.com>
2026-05-12 11:18:53 +0200

    feat: create a program named 1-stdin.js that will be executed through command line

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,8 @@
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdin.resume();
+process.stdin.on('data', (data) => {
+  process.stdout.write('Your name is: ' + data.toString());
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
