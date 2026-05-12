3755f7ff17c20dbd20a3f415526a2078ddeb011c
serkam31 <mmateomarques@gmail.com>
2026-05-12 10:54:26 +0200

    fix: format code style for displayMessage function and module.exports

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,4 +1,4 @@
-function displayMessage(message){
-    console.log(message)
+function displayMessage(message) {
+  console.log(message);
 }
-module.exports = displayMessage
+module.exports = displayMessage;
